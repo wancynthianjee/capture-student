568393fef75a528b2ad898a6e3734547aaaf0cc6
wancynthianjeri <wanjirucynthianjeri@gmail.com>
2023-02-03 10:51:39 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+fun main(arg: Array<String>) {
+ var fullName = "Peris Wangui"
+ println(fullName)
+
+ var age = 17
+ println(age)
+
+ var phoneNumber = "0713247749"
+ println(phoneNumber)
+
+ var weightInkg = 52
+ println(weightInkg)
+
+  var ugandaCitizen: Boolean  = false
+ println(ugandaCitizen)
+
+ }
+
+
+
+
